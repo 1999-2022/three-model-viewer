99fded328f12120cd91b7dd3148b2961f8d8387e
Alan <tr08td3@protonmail.ch>
2021-11-05 22:06:30 +0100

    cubee

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Suspense, useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { ContactShadows } from '@react-three/drei'
 import Model from './Model'
+import { PointLight } from 'three'
 
 function Rig({ children }) {
   const ref = useRef()
@@ -15,11 +16,48 @@ function Rig({ children }) {
 
 export default function Viewer() {
   const ref = useRef()
+
+  // should move these funcs to utils
+  const createCanvasTexture = (draw) => {
+    const canvas = document.createElement('canvas'),
+      ctx = canvas.getContext('2d')
+    draw(ctx, canvas)
+    return new THREE.CanvasTexture(canvas)
+  }
+
+  const createEmissiveMap = () => {
+    let COLOR_EMISSIVE_MAP_FRONT = new THREE.Color(1, 1, 1)
+    return createCanvasTexture(function (ctx, canvas) {
+      ctx.strokeStyle = COLOR_EMISSIVE_MAP_FRONT.getStyle()
+      ctx.strokeRect(1, 1, canvas.width - 1, canvas.height - 1)
+    })
+  }
+
   return (
     <Canvas camera={{ position: [0, -10, 65], fov: 50 }} dpr={[1, 2]}>
+      <color attach="background" args={"black"} />
       <pointLight position={[100, 100, 100]} intensity={0.8} />
-      <hemisphereLight color="#ffffff" groundColor="#b9b9b9" position={[-7, 25, 13]} intensity={0.85} />
       <Suspense fallback={null}>
+        <group position={[0, 10, 0]}>
+          <Rig>
+            <mesh
+              position={[3, -5, -22]}
+              rotation={[0, -0.2, 0]}
+              geometry={
+                new THREE.BoxGeometry(25, 50, 25)
+              }
+              material={
+                new THREE.MeshStandardMaterial({
+                  color: new THREE.Color(0xffffff)
+                })
+              }
+            />
+          </Rig>
+          <PointLight
+            position={[3, -5, -22]}
+            rotation={[0, -0.2, 0]}
+          />
+        </group>
         <group position={[0, 10, 0]}>
           <Rig>
             <Model />
@@ -31,7 +69,7 @@ export default function Viewer() {
             width={100}
             height={100}
             blur={3}
-            far={50}
+            far={1000}
           />
         </group>
       </Suspense>
